4a7f5a5efddd1cc2b0ba1f938271315bdc31318c
iwai-kentaro <kentaro.wdc@gmail.com>
2024-08-23 10:43:29 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const e = require('express');
+const express = require('express');
+const path = require("path");
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.post("/api/v1/quiz", function(req,res){
+    const answer = req.body.answer;
+    if(answer === "2"){
+        // res.send("<h1>正解！</h1>");
+        res.redirect("/correct.html");
+    }else{
+        // res.send("不正解");
+        res.redirect("/wrong.html");
+    }
+})
+
+// app.get('/', function (req, res) {
+//   res.send('<h1>top!!</h1>')
+// })
+// app.get('/about', function (req, res) {
+//   res.send({
+//     name: "Make",
+//     age: 34,
+//   })
+// })
+
+app.listen(4000, function(){
+    console.log("I am running");
+})
